824cb097281e1af58cfd345723903dc26c9e5303
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2020-11-02 20:59:50 +0000

    Updated dashboard with phenotype ontologies

--- run-dash.sh
+++ run-dash.sh
@@ -4,7 +4,6 @@ set -e
 
 OBODASH="sh odk.sh obodash"
 
-rm -rf dashboard
+#rm -rf dashboard
 #$OBODASH refresh -B
-$OBODASH all ROBOT=robot ROBOT_JAR=/tools/robot.jar -B
-$OBODASH truncate_reports_for_github REPORT_LENGTH_LIMIT=200 -B
+$OBODASH -C dashboard-config.yml
